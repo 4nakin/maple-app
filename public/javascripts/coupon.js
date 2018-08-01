f8190c350601a0d325c9ade7116557ebcad67496
Jessica Erazo <jessicamerazo@gmail.com>
2018-08-01 01:11:20 -0700

    removed accept type to all images

--- public/javascripts/coupon.js
+++ public/javascripts/coupon.js
@@ -246,7 +246,7 @@ function renderEditModal() {
                                     current image
                                   </div>
                                   <label for="couponImage">Upload an image of your coupon <span class ="limitsOnInputs">(only accepts png/jpeg)</span></label>
-                                  <input id="couponImage" type="file" name="couponImage" accept="image/*"/>
+                                  <input id="couponImage" type="file" name="couponImage"/>
                                   <label for="couponImage" class="custom-file-upload"></label>
                                 </div>
 
